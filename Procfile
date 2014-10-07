07fd23764dcb33f1ddcb9ee522b817fcab7422ff
Jks Liu <chinatianma@gmail.com>
2014-10-08 07:51:17 +0800

    Add init cname30x files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/cname30x-standalone.jar clojure.main -m cname30x.web
---
+++ test/cname30x/web_test.clj
@@ -0,0 +1,6 @@
+(ns cname30x.web-test
+  (:require [clojure.test :refer :all]
+            [cname30x.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
